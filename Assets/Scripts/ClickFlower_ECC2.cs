aa8ab44cb527e9fa4afdd5697239019d9a46e64b
OSSU <o.ss.u2056@gmail.com>
2024-06-18 23:05:54 +0900

    fix: 스탬프 획득 수정

--- Assets/Scripts/ClickFlower_ECC2.cs
+++ Assets/Scripts/ClickFlower_ECC2.cs
@@ -14,6 +14,7 @@ public class ClickFlower_ECC2 : MonoBehaviour
         yield return new WaitForSeconds(2.0f);
 
         string currentSceneName = SceneManager.GetActiveScene().name;
+        SetNavigateEnd();
 
         if (currentSceneName == "AR_ECC1")
         {
@@ -30,6 +31,26 @@ public class ClickFlower_ECC2 : MonoBehaviour
         else if (currentSceneName == "AR_Library")
         {
             SceneManager.LoadScene("inside_Library");
+        }
+    }
+
+    private void SetNavigateEnd()
+    {
+        if(SceneManager.GetActiveScene().name == "AR_ECC1")
+        {
+            InsideSceneManager.manager.starbucks = true;
+        }
+        else if(SceneManager.GetActiveScene().name == "AR_ECC2")
+        {
+            InsideSceneManager.manager.ecc_stairs = true;
+        }
+        else if(SceneManager.GetActiveScene().name == "AR_Library")
+        {
+            InsideSceneManager.manager.library = true;
+        }
+        else if (SceneManager.GetActiveScene().name == "AR_ENG")
+        {
+            InsideSceneManager.manager.gong_b2 = true;
         }
     }
 }
